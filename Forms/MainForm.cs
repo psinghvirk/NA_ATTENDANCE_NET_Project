6079444000feb820c87b7e7389d15408bfc4f00f
psinghvirk <53937757+psinghvirk@users.noreply.github.com>
2025-05-12 23:25:19 +0800

    Update MainForm.cs

--- Forms/MainForm.cs
+++ Forms/MainForm.cs
@@ -1,1 +1,13 @@
-// MainForm code placeholder
+using System;
+using System.Windows.Forms;
+
+namespace NA_Attendance_App
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
